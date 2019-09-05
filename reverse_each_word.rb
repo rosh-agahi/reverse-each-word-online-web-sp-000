7f7c8ed20f72ff30a35816796ecb0ae910436323
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 16:29:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
-def reverse_each_word(statement)
-  statement.split(" ").collect do |word|
-    puts word.reverse
+def reverse_each_word(sentence)
+  list_of_words = []
+  sentence.split(" ").collect do |word|
+    list_of_words << word.reverse
   end
+  list_of_words.join(" ")
 end
 
   
